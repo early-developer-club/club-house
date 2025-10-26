4d1567b7ed9dfd00f14a15e7966a7bae58708b10
da-in <talown@naver.com>
2025-10-26 20:04:47 +0900

    chore: Update site description for clarity and engagement
    - Revised the site description to better reflect the community's focus on experiencing and applying new technologies in a collaborative environment.

--- src/config/site.ts
+++ src/config/site.ts
@@ -2,7 +2,7 @@ import { SiteConfig } from "@/types"
 
 export const siteConfig: SiteConfig = {
   name: "Early Developer Club",
-  description: "초보 개발자들을 위한 커뮤니티 클럽",
+  description: "새로운 기술을 함께 즐겁게 경험하고 삶에 적용하는 개발자 커뮤니티",
   url: "https://early-developer-club.vercel.app",
   ogImage: "https://early-developer-club.vercel.app/og.jpg",
   links: {
